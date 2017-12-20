fcb1017724d9e5d470de91668da961dea5e49372
James Tharpe <james@brainbytescreative.com>
2017-12-20 10:17:54 -0500

    Adds handler spec stub

--- test/handler.spec.ts
+++ test/handler.spec.ts
@@ -1,25 +1,28 @@
-
 // Test framework
 import * as mocha from 'mocha';
 import * as chai from 'chai';
-import * as asPromised from 'chai-as-promised'
+// import * as asPromised from 'chai-as-promised'
+
+// AWS Lambda
+import { Handler, Callback, Context } from 'aws-lambda';
 
 // System under test
 import { submitForm } from '../handler';
 
 // Wiring
-chai.use(asPromised);
+// chai.use(asPromised);
 const expect = chai.expect;
+const should = chai.should();
 
 // Tests
 
 describe("handler", () => {
   describe("submitForm", () => {
-    let callback = (error, response) => {
-      console.log(error);
-      console.log(response);
-    }
-    submitForm(null, null, callback);
-    it("should wire-up services");
+    it("should wire-up services", () => {
+      submitForm(null, null, (error : Error, result : any) => {
+        expect(error).to.be.null;
+        result.body.should.equal('{"message":"Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!","input":null}');
+      })
+    });
   });
 });
