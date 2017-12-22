424cec77813dfe4b38aeccc81427ed74ddd4b25a
James Tharpe <james@brainbytescreative.com>
2017-12-22 14:06:39 -0500

    Correctly handles errors

--- src/aws/handler.ts
+++ src/aws/handler.ts
@@ -5,21 +5,21 @@ import { ses } from './sender';
 import { html } from '../formatter';
 
 export const submitForm : Handler = async (event : APIGatewayEvent, context : Context, cb : Callback) => {
+  const headers = { "Access-Control-Allow-Origin" : "*" }; // Required for CORS support to work
   try{
     console.log("Received event:", JSON.stringify(event, null, 2));
     await Service.submitForm(qs.parse(event.body), ses, html);
-    const response = {
+    cb(null, {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      },
-      body: JSON.stringify({
-        message: "Success"
-      })
-    }
-    cb(null, response);
+      headers: headers,
+      body: JSON.stringify({ message: "Success" })
+    });
   }
   catch(err){
-    cb(err);
+    cb(null, {
+      statusCode: 500,
+      headers: headers,
+      body: JSON.stringify({ error: err })
+    });
   }
 }
